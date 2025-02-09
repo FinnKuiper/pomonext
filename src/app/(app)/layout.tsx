0624d751c2f0f3df51553afc5eae39a6fbc4f81e
Finn Kuiper <finnkuiper05@gmail.com>
2025-02-09 20:50:11 +0100

    refactor layout component to establish database connection; simplify structure by removing QueryClientProvider

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,19 +1,15 @@
-"use client";
 import NavBar from "@/components/app/NavBar";
+import connectDB from "@/lib/mongoose";
 import React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
-
-export default function layout({
+export default async function layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  await connectDB();
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <main className="p-4">{children}</main>
-        <NavBar />
-      </QueryClientProvider>
+      <main className="p-4">{children}</main>
+      <NavBar />
     </>
   );
 }
